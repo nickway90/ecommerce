50a6f715b3e984358a1d0489997627ad11b318cc
nickway90 <king.nickway90@gmail.com>
2018-10-25 21:16:47 -0600

    updated item template

--- shopping/views.py
+++ shopping/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 
@@ -14,11 +14,10 @@ def cart(request):
 
 
 def items(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, 'items/index.html', context)
+    items = Item.objects.all()
+    return render(request, 'items/index.html', {'items': items})
 
 
 def item(request, id):
-    return render(request, 'item/index.html')
+    item = get_object_or_404(Item, pk=id)
+    return render(request, 'item/index.html', {'item': item})
