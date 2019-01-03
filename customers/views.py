b2bb8e73bc7e0d0aedc33023dd2e633ab8b24801
nickway90 <king.nickway90@gmail.com>
2019-01-02 21:27:57 -0700

    successfully creating customers and logging in

--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login/index.html')
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['email'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'login/index.html')
+    return redirect('../shopping')
 
 
 def register(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(
+            request.POST['email'],
+            request.POST['email'],
+            request.POST['password'])
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.save()
     return render(request, 'register/index.html')
