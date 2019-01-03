163a1aa482e0018e3ad16d51338798b61e62bf9d
nickway90 <king.nickway90@gmail.com>
2019-01-02 21:06:33 -0700

    created register route and updated login

--- customers/views.py
+++ customers/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'login/index.html')
+
+
+def register(request):
+    return render(request, 'register/index.html')
