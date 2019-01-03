d0bcf4911815973f994d72102a6c5e1071478514
nickway90 <king.nickway90@gmail.com>
2019-01-02 20:53:21 -0700

    created login route

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shopping/', include('shopping.urls'))
+    path('shopping/', include('shopping.urls')),
+    path('customers/', include('customers.urls'))
 ]
