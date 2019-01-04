f711661513a9db735fd7381baba7fb50f7ec46e6
nickway90 <king.nickway90@gmail.com>
2019-01-03 19:39:45 -0700

    adding primary and last used at fields to addresses

--- customers/models.py
+++ customers/models.py
@@ -15,10 +15,12 @@ class UserAddress(models.Model):
         User, on_delete=models.CASCADE, related_name="addresses")
     type = models.ForeignKey(
         AddressType, on_delete=models.SET_NULL, null=True, related_name="addresses")
+    primary = models.BooleanField(default=False)
     street_1 = models.CharField(max_length=300)
     street_2 = models.CharField(max_length=300, null=True)
     city = models.CharField(max_length=300)
     state = models.CharField(max_length=150)
     postal_code = models.CharField(max_length=100)
     telephone = models.CharField(max_length=100)
+    last_used_at = models.DateTimeField(null=True)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
