888ce7c59c86d818d83b0e051b89113c75849348
nickway90 <king.nickway90@gmail.com>
2019-01-03 19:33:18 -0700

    created address and type models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class AddressType(models.Model):
+    type = models.CharField(max_length=100)
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="addresses")
+    type = models.ForeignKey(
+        AddressType, on_delete=models.SET_NULL, null=True, related_name="addresses")
+    street_1 = models.CharField(max_length=300)
+    street_2 = models.CharField(max_length=300, null=True)
+    city = models.CharField(max_length=300)
+    state = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
