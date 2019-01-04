0c9c57e65172250337b5129258b23e04ac0e057c
nickway90 <king.nickway90@gmail.com>
2019-01-03 21:17:35 -0700

    created order models

--- shopping/models.py
+++ shopping/models.py
@@ -51,3 +51,45 @@ class CartItem(models.Model):
 
     def __str__(self):
         return '{} ({})'.format(self.item.name, self.qty)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="order", null=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    total = models.FloatField()
+
+    def from_cart(self, cart):
+        self.customer = cart.customer
+        self.total = cart.total
+        for cart_item in cart.cart_items:
+            order_item = OrderItem()
+            order_item.from_cart_item(cart_item)
+            self.order_items_set.add(order_item)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_items")
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    qty = models.IntegerField(default=0)
+    total = models.FloatField()
+
+    def from_cart_item(self, cart_item):
+        self.item = cart_item.item
+        self.sku = cart_item.item.sku
+        self.name = cart_item.item.name
+        self.description = cart_item.item.description
+        self.price = cart_item.item.price
+        self.qty = cart_item.qty
+        self.total = cart_item.total
+
+
+class OrderTransaction(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_transactions")
+    transaction_id = models.CharField(max_length=150)
