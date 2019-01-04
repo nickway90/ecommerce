323e508d0934ca435a0e26db614b619acd290608
nickway90 <king.nickway90@gmail.com>
2019-01-03 20:35:23 -0700

    updated register to use forms

---
+++ customers/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from .models import AddressType, UserAddress
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password']
+
+
+class UserAddressForm(ModelForm):
+    class Meta:
+        model = UserAddress
+        fields = ['type', 'primary', 'street_1', 'street_2',
+                  'city', 'state', 'postal_code', 'telephone']
--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login
+from .forms import UserForm
 
-# Create your views here.
 
 def login(request):
     if request.method == 'POST':
-        user = authenticate(username=request.POST['email'], password=request.POST['password'])
+        user = authenticate(
+            username=request.POST['email'], password=request.POST['password'])
         if user is not None:
             auth_login(request, user)
             return redirect('../shopping')
@@ -15,11 +16,16 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        user = User.objects.create_user(
-            request.POST['email'],
-            request.POST['email'],
-            request.POST['password'])
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.save()
-    return render(request, 'register/index.html')
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            user = User.objects.create_user(
+                request.POST['email'],
+                request.POST['email'],
+                request.POST['password'])
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.save()
+    else:
+        form = UserForm()
+    return render(request, 'user/create.html', {'form': form})
